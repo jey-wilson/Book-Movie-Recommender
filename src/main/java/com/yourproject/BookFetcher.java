f33b0944abc05fe83eb8d55019225d7ed71ff458
Jeyla <Jeyla@DESKTOP-9CGGBOO.mynetworksettings.com>
2025-02-28 18:05:21 -0500

    After making class files

---
+++ src/main/java/com/yourproject/BookFetcher.java
@@ -0,0 +1,14 @@
+/* Jeyla Wilson 02/28/2025
+ * holds and gets data for the book title given as user input in main
+ * extracts title, author, and subjects from the Open Library API JSON info */
+package com.yourproject;
+
+class BookFetcher{
+	
+	// declare variables that need extracted from the JSON
+	private String bookTitle;
+	private String author;
+	private String subjects;
+	
+	
+}
--- src/main/java/com/yourproject/Main.java
+++ src/main/java/com/yourproject/Main.java
@@ -5,12 +5,21 @@ import okhttp3.Request;
 import okhttp3.Response;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Scanner;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Main {
     public static void main(String[] args) {
-        try {
+    	// code from testing if the movie API worked
+    	/*
+    	 * try {
             OkHttpClient client = new OkHttpClient();
 
+           
+            
             Request request = new Request.Builder()
                     .url("https://api.themoviedb.org/3/movie/popular?api_key=302dc57bca82ca1e3014c074c55dfbb9")
                     .build();
@@ -26,6 +35,36 @@ public class Main {
 
         } catch (Exception e) {
             e.printStackTrace();
+            
         }
+        */
+    	
+    	// get user input for title and author of a book they like
+    	Scanner scan = new Scanner(System.in);
+    	System.out.print("Enter a book title: ");
+    	String bookTitle = scan.nextLine();
+    	System.out.print("Enter the author: ");
+    	String authorName = scan.nextLine();
+    	
+    	//fetch the book data from open library API
+    	try {
+    	URL url = new URL("https://openlibrary.org/search.json?title=" + bookTitle + "&author=" + authorName);
+    	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    	conn.setRequestMethod("GET");
+    	BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    	StringBuilder response = new StringBuilder();
+    	String line;
+    	while ((line = reader.readLine()) != null) {
+    	    response.append(line);
+    	}
+    	reader.close();
+    	}
+    	catch (Exception e){
+    		e.printStackTrace();
+    	}
+    	
+    	scan.close();
+
     }
+    
 }
